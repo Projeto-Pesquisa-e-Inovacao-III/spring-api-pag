89b1e5ef9c8a6569dfa87fc3f6a3e60f7bdb7dbe
JoaoGaldinoCunha <joaovictorgacunha@gmail.com>
2026-03-23 00:54:27 -0300

    fix:message convert

--- src/main/java/com/csf/pagmodulo/infrastructure/config/RabbitMqConfig.java
+++ src/main/java/com/csf/pagmodulo/infrastructure/config/RabbitMqConfig.java
@@ -1,15 +1,15 @@
 package com.csf.pagmodulo.infrastructure.config;
 
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
+
     @Bean
     public MessageConverter messageConverter() {
-        return new SimpleMessageConverter() {
-        };
+        return new JacksonJsonMessageConverter();
     }
 }
